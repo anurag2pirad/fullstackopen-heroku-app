d28b0f4a3ed68fe296e681402b67121a67c9cf4b
Anurag Roy <anurag@2pirad.com>
2020-06-02 23:06:59 +0530

    Initial setup

---
+++ build/precache-manifest.09b7e02903d8b431b2c0ab3d7d6bb11c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ed7f76ba28fbc403ae5388d65074a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da9c83f8379307f89c63",
+    "url": "/static/js/2.6de0d325.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6de0d325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0fa8ad4f5f75866641",
+    "url": "/static/js/main.0f7920e1.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,104 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.static("build"));
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :postContent"
+  )
+);
+
+morgan.token("postContent", (req, res) => {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body);
+  }
+});
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4,
+  },
+];
+
+app.get("/info", (req, res) => {
+  const requestDateTime = new Date().toString();
+  res.send(
+    `<div>
+        Phonebook has info for ${persons.length} people <br /> <br />
+        ${requestDateTime}
+     </div>`
+  );
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.use(express.json());
+
+app.post("/api/persons", ({ body }, res) => {
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "Name or number is missing",
+    });
+  }
+
+  if (persons.find((p) => p.name === body.name)) {
+    return res.status(400).json({
+      error: "Name must be unique",
+    });
+  }
+
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 1000),
+  };
+  persons = persons.concat(newPerson);
+
+  res.json(newPerson);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((p) => p.id !== id);
+
+  res.status(204).end();
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Express app started on port ${PORT}`);
+});
